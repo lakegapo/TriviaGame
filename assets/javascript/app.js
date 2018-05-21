878712469b2465486d3f8404ebf226b35b08164e
Fletcher Bunales <lakegapo@yahoo.com>
2018-05-20 21:48:07 -0700

    Starter

---
+++ assets/javascript/app.js
@@ -0,0 +1,45 @@
+
+// Array of questions
+var questions = [
+    "Who was the MVP of the 1985 Finals?",
+    "Who scored on Magic Johnson's record-breaking assist?",
+    "Who was the coach when the Lakers won the 1980 NBA Finals?",
+    "Which Laker won the Defensive Player of the Year as a reserve?",
+    "Which vacation destination did Nick Van Exel yell out to break a huddle?",
+    "The Laker band is comprised of students from which school?",
+    "Who was traded to the Charlotte Hornets for Kobe Bryant?",
+    "Who succeeded Pat Riley as Lakers coach?",
+    "Which player lied about getting injured in a snowboarding accident?",
+    "What country is Sasha Vujacic from?"
+];
+// Arry of the correct answers
+var correctAnswers = [
+    "Kareem Abdul-Jabber",
+    "Terry Teagle",
+    "Paul Westhead",
+    "Michael Cooper",
+    "Cancun",
+    "USC",
+    "Vlade Divac",
+    "Mike Dunleavy",
+    "Vladimir Radmanovic",
+    "Slovenia"
+];
+// Array of the answer choices
+var answersList = [
+    ["Magic Johnson","Kareem Abdul-Jabber","Jamaal Wilkes","Bob McAdoo"],
+    ["Terry Teagle","Byron Scott", "James Worthy","Orlando Woolridge"],
+    ["Jerry West","Pat Riley","Paul Westhead","Billy Cunningham"],
+    ["Lamar Odom","Kurt Rambis","Michael Cooper","Rick Fox"],
+    ["Maui","Cancun","Fiji","Bora Bora"],
+    ["UCLA","USC","Cal State LA","Drew University"],
+    ["Nick Van Exel","Anthony Peeler","Elden Campbell","Vlade Divac"],
+    ["Mike Dunleavy","Randy Pfund","Bill Bertka","Phil Jackson"],
+    ["Lamar Odom","Vladmir Radmanovic","Nick Van Exel","Sedale Threatt"],
+    ["Croatia","Serbia","Slovenia","Slovakia"]
+];
+//variable that will replace the div "game-area"
+var gameArea;
+
+
+
